92ad17d931b38b7b61bcb58c1dca8c24ec006761
Niko <nikolas.urcuyo@gmail.com>
2023-10-09 18:25:25 -0400

    all new data in place, comments added

--- main.py
+++ main.py
@@ -11,15 +11,13 @@ with open("U.S. Presidents Birth and Death Information - Sheet1.csv", 'r') as f1
         reader = csv.reader(f1)
         writer = csv.writer(f2)
         header = next(reader)
-        writer.writerow(header + ["year_of_birth","lived_years","lived_months","lived_days"])
+        writer.writerow(header + ["year_of_birth","lived_years","lived_months","lived_days"])  # add new headers
         for line in reader:
             towrite = line
             yob = line[1][-5:]
-            if yob != '':
+            if yob != '':  # catches ref lines at the end without needing "try/except" blocks
                 start_date = dp.parse(line[1])
-                end_date = dt.now() if line[3] == '' else dp.parse(line[3])
-                diff = dr.relativedelta(end_date, start_date)
-                print(diff)
+                end_date = dt.now() if line[3] == '' else dp.parse(line[3]) # if alive, cmp birth to now, else to death
+                diff = dr.relativedelta(end_date, start_date)  # gives diff in y, m, d, etc.
                 towrite = (line + [yob, diff.years, diff.months+diff.years*12, int(str(end_date-start_date)[:5])])
-
             writer.writerow(towrite)
